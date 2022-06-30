5fc8caf8677eaa8a798aa47d1c6483dbd75e7c90
TheDoomfire <90956505+TheDoomfire@users.noreply.github.com>
2022-07-01 00:34:10 +0200

    Smått o gott

--- job.py
+++ job.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 
+#{}
 arbetsformedlingen = "https://arbetsformedlingen.se/platsbanken/annonser?l=3:kuMn_feU_hXx&page=1"
 
 minsida = "https://arbetsformedlingen.se/platsbanken/annonser?l=3:kuMn_feU_hXx&page={}"
-jobs = ["lager", "truckförare"]
+mina_yrken = ["lager", "truckförare", "data"]
 max_antal_sidor = 2
 
 
@@ -19,8 +20,8 @@ jobs = r.html.find("pb-feature-search-result-card")
 for item in jobs:
     jobs = {
         "job": item.find("a")[0].text,
-        "job_titel": item.find("div")[5].text,
-        "time": item.find("div")[6].text,
+        "yrke": item.find("div")[5].text,
+        "tid": item.find("div")[6].text,
         "link": item.absolute_links
     }
     print(jobs)
@@ -33,13 +34,3 @@ for page in range(1,max_antal_sidor+1):
     soup = BeautifulSoup(html_text, 'lxml')
     print(url)
  """
-
-
-
-""" html_text = requests.get(arbetsformedlingen)
-#soup = BeautifulSoup(html_text, "lxml")
-doc = BeautifulSoup(html_text.text, "html.parser")
-
-cards = doc.find_all("h3")
-
-print(cards) """
