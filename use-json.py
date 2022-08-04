5b12ecfc3c589a756be14e33ac49011400d2ec3d
TheDoomfire <90956505+TheDoomfire@users.noreply.github.com>
2022-08-04 14:57:01 +0200

    Can read the json
    But cant use it yet...

--- use-json.py
+++ use-json.py
@@ -1,11 +1,13 @@
 import json
+from jsonschema import validate
 
 
 theFile = "output.json"
 
 
-with open("output.json", "r", encoding="utf8") as f: # "r" is default
-    data = json.load(f)
+""" with open("output.json", "r", encoding="utf8") as f: # "r" is default
+    data = json.load(f) """
+
 
 """ data = [json.loads(line)
         for line in open('output.json', 'r', encoding='utf-8')]
@@ -13,3 +15,24 @@ with open("output.json", "r", encoding="utf8") as f: # "r" is default
 for i in data:
     if i["addressLocality"] == "Örebro":
         print(i) """
+
+
+# Need to format/validate json on read it like this.
+
+""" def validateJsonFile(jsonFile):
+    try:
+        json.load(jsonFile)
+    except ValueError as err:
+        print(err)
+        return False
+    return True
+
+
+with open(theFile, encoding="utf8") as f:
+    print("Json Validated: ", validateJsonFile(f)) """
+
+data = [json.loads(line) for line in open(theFile,'r', encoding="utf8")]
+
+for i in data['originalJobPosting']:
+    print(i)
+
