402d6a25e216a9e36adcacad016cbf44b1125d27
TheDoomfire <90956505+TheDoomfire@users.noreply.github.com>
2022-10-02 14:13:10 +0200

    Update read-from-csv.py

--- read-from-csv.py
+++ read-from-csv.py
@@ -8,9 +8,9 @@ with open("job-to-apply.csv") as f:
     next(reader) # Skips the header row
 
     for row in reader:
-        webbrowser.open_new(row[4])
+        # webbrowser.open_new(row[4])
         print(row[4])
-        if count > 6:
+        if count > 20:
             break
         count += 1
 
