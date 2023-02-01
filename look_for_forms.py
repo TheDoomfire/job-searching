97020a96d22aa628bcc9a0bfba1998b89a1331e2
TheDoomfire <90956505+TheDoomfire@users.noreply.github.com>
2023-02-01 21:43:41 +0100

    Looking for the form.
    Found it in some websites at least.

--- look_for_forms.py
+++ look_for_forms.py
@@ -1,9 +1,14 @@
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import json
+import time
 
 
-the_url = "https://apply.recman.no/job_post.php?id=314240&path=ams&sub_id=462"
+the_url = "https://performiq.se/lediga-jobb/6998/"
+
+
+# https://performiq.se/lediga-jobb/6998/
+# https://trv.heroma.se/prod/trvpp01/externwebbv2/LedigaJobb/EW2PageJobPostingAdvert.aspx?jobpostingid=7277691825931409813
 
 
 """ buttons = soup.find_all('button')
@@ -23,10 +28,21 @@ else:
 # look for button, or a tag with the text "ansök" + more
 # take that link and run the function on it. If its not the same as the link already provided.
 # If not found anything, save the link somewhere. So I can later look myself for it.
+# If found and sent, change its applied for somewhere.
+# Move to next url
 # <form
 
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+    'Accept-Language': 'en-GB,en;q=0.5',
+    'Referer': 'https://www.google.com', # Where we came from
+    'DNT': '1' # Do Not Track
+}
+
+
 # Maybe make it compare to .lower()
-btn_text = ["ansök", "Ansök", "apply", "Apply", "sök tjänst", "Sök Tjänst", "Sök tjänst"]
+btn_text = ["ansök", "Ansök", "apply", "Apply", "sök tjänst", "Sök Tjänst", "Sök tjänst", "sök jobbet", "Sök jobbet", "Sök jobbet här"]
 
 
 with open('form_data.json', 'r') as f:
@@ -37,11 +53,14 @@ form_text_data = {
     "name": form_data["name"],
     "namn": form_data["name"],
     "first_name": form_data["name"],
+    "firstName": form_data["name"],
     "lastname": form_data["last_name"],
     "last_name": form_data["last_name"],
+    "surName": form_data["last_name"],
     "email": form_data["email"],
     "phone-prefix": form_data["phone_prefix"],
     "phone": form_data["phone"],
+    "mobilePhone": form_data["phone"],
     "phone_number": form_data["phone"],
     "address": form_data["address"]
 }
@@ -51,20 +70,22 @@ with open('Albin Mustafa - CV.pdf', 'rb') as cv_file, open('Albin Mustafa - CV.p
     form_files = {
         'cv': cv_file,
         'personal_letter': letter_file,
-        'file[]': letter_file
+        'file[]': letter_file,
+        'other-document': letter_file
     }
 
 
 hej = 0
+submit_input = None
 def form_search(url):
     s = HTMLSession()
-    r = s.get(url)
+    r = s.get(url, headers=headers)
     r.html.render(sleep=1)
     soup = BeautifulSoup(r.text, 'html.parser')
 
     forms = soup.find_all('form')
     buttons = soup.find_all('button')
-    links = soup.find_all('link')
+    links = soup.find_all('a')
 
     if forms:
         for form in forms:
@@ -73,17 +94,41 @@ def form_search(url):
                 inputs = form.find_all('input', {'type': 'file'})
                 if len(inputs) >= 2:
                     print("Form with minimum of 2 file input fields found!")
-                    action_url = form['action']
+                    # action_url = form['action']
                     input_fields = form.find_all('input')
+                    # JUST FOR TESTING
+                    button = soup.find('button', text='Ansök')
+                    if button:
+                        parent_form = button.find_parent('form')
+                        if parent_form:
+                            inputs = parent_form.find_all('input')
+                            payload = {input['name']: input.get('value', '') for input in inputs}
+                            print(payload) # Finds our all the data that needs to be sent.
+
                     # Prepare the payload data
                     # payload = {field['name']: field.get('value', '') for field in input_fields}
                     # Submit the form
                     # response = s.post(action_url, data=payload)
-                    break 
+                    break
+            for value in btn_text:
+                submit_input = soup.find('input', {'type': 'submit', 'value': value})
+                if submit_input:
+                    print("found a submit with value")
+                    break
     elif buttons:
         for button in buttons:
             if button.text.strip() in btn_text: # Maybe make it compare to .lower()
                 print("Button with text '{}' found!".format(button.text.strip()))
+                print("BUTTON")
+                break
+    if links:
+        for link in links:
+            if link.text.strip() in btn_text: # Maybe make it compare to .lower()
+                print("Link with text '{}' found!".format(link.text.strip()))
+                link_href = link.get('href')
+                print("Link Href:", link_href)
+                time.sleep(5) # Need to have a sleep otherwise may get problems with headers.
+                form_search(link_href)
                 break
 
 
